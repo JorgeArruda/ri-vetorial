c364182465d61e7a72f9d8b6d7f03f0b2a52ce53
JorgeArruda <arrudajorge49@gmail.com>
2018-05-05 18:19:25 +0000

    Read pdf

--- pdf_read.py
+++ pdf_read.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from docx import Document
+import pdftotext
+
 
 def files_folder(path):
     if type(path) != str:
@@ -9,18 +10,18 @@ def files_folder(path):
     files = os.listdir(path)
     documents = {}
     for file in files:
-        text = Document(path+file)
+        pdf = pdftotext.PDF(open(path+file, "rb"))
         full_text = ''
-        for p in text.paragraphs: 
-            full_text = full_text+p.text
-        documents[file] = full_text            
+        for page in pdf:
+            full_text = full_text+page
+        documents[file] = full_text           
     return documents
 
 def file(name):    
     if type(name) != str:
         return "Erro, argument name != string"
-    text = Document(name)
+    pdf = pdftotext.PDF(open(name, "rb"))
     full_text = ''
-    for p in text.paragraphs: 
-        full_text = full_text+p.text
+    for page in pdf:
+        full_text = full_text+page
     return full_text
