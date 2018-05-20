e5b30b120ccc5d991045dc626e405b03e173f5f2
JorgeArruda <arrudajorge49@gmail.com>
2018-05-20 17:20:53 +0000

    Term frequency, log normalization and double normalization 0.5

--- archive.py
+++ archive.py
@@ -3,12 +3,13 @@
 import json
 
 # Importando arquivos
-import my_app.lex as lex
-import my_app.ocr as ocr
-import my_app.pdf_read as pdf_read
-import my_app.docx_read as docx_read
-import my_app.html_read as html_read
+import lex as lex
+import ocr as ocr
+import pdf_read as pdf_read
+import docx_read as docx_read
+import html_read as html_read
 from operator import itemgetter
+
 
 def sort_dic(dic, indice=0):
     return sorted(dic.items(), key=itemgetter(indice))
@@ -37,40 +38,89 @@ def get_tokens(text, language='portuguese'):
         return "Erro, argument text != string"
     return lex.tokenize(text, language)
 
-def get_frequency(listTokens, frequencyWord):
-        "Vare a lista de tokens do documento, retorna a frequência. Incrementa também a frequência global. return (frequencyDocument, frequencyWord)"
+def remove_stopwords( tokens ):
+    "Remove stopwords e verifica a quantidade removida. Return {'tokens', 'qt_stopwords', 'qt_stopwords_total', 'qt_adverbios', 'qt_adverbios_total'}"
+    if ( type(tokens) != dict ):
+        print( '\n---Tokens não é uma lista!', tokens)
+    if ( len(tokens) == 0 ):
+        print( '\n---Lista de tokens vazia!', tokens)
 
-        frequencyDocument = {}
-        for token in listTokens:
-            print(token)
-            if token in frequencyWord:
-                frequencyWord[ token ]+=1
-            else:
-                frequencyWord[ token ]=1
+    qt_stopwords = qt_stopwords_total = 0
+    qt_adverbios = qt_adverbios_total = 0
+    qt_tok_total = qt_tok = 0
 
-            if token in frequencyDocument:
-                frequencyDocument[ token ]+=1
-            else:
-                frequencyDocument[ token ]=1
+    max_tf = 0
+    new_tokens = {}
+
+    if ( not(type(tokens) != dict or len(tokens) == 0) ):
+        stopwords = stop().stopwords
+        adverbios = stop().adverbios
         
-        return (dict(sort_dic(frequencyDocument)), dict(sort_dic(frequencyWord)))
-
-def get_frequency2(listTokens, frequencyWord):
-        "Vare a lista de tokens do documento, retorna a frequência. Incrementa também a frequência global. return (frequencyDocument, frequencyWord)"
-        frequencyDocument = {}
-        # if type(frequencyWord)=='list':
-        for token in listTokens:
-            if token in frequencyWord:
-                frequencyWord[ token ]+=1
+        for key in tokens:
+            qt_tok_total += tokens[key]
+            qt_tok += 1
+            if ( key in stopwords ):
+                qt_stopwords += 1
+                qt_stopwords_total += tokens[key]
+                # tokens.pop(key)
+            elif ( key in adverbios ):
+                qt_adverbios += 1
+                qt_adverbios_total += tokens[key]
+                # tokens.pop(key)
             else:
-                frequencyWord[ token ]=1
+                if tokens[key] > max_tf:
+                    max_tf = tokens[key]
+                new_tokens[key] = tokens[key]
+        print( '\n\t%d Stopwords e %d adverbios removidos.' % (qt_stopwords_total, qt_adverbios_total) )
+    return {'tokens': new_tokens,\
+        'qt_stopwords':qt_stopwords, 'qt_stopwords_total': qt_stopwords_total,\
+        'qt_adverbios': qt_adverbios, 'qt_adverbios_total': qt_adverbios_total,\
+        'qt_tok': qt_tok, 'qt_tok_total': qt_tok_total, 'max':max_tf }
+    
 
-            if token in frequencyDocument:
-                frequencyDocument[ token ]+=1
-            else:
-                frequencyDocument[ token ]=1
-        
-        return (dict(sort_dic(frequencyDocument)), frequencyWord)
-            
-if __name__ =="__main__":
+def get_frequency(listTokens):
+    "Vare a lista de tokens do documento, retorna a frequência. return frequencyDocument"
+    frequencyDocument = {}
+    for token in listTokens:
+        print(token)
+        if token in frequencyDocument:
+            frequencyDocument[ token ]+=1
+        else:
+            frequencyDocument[ token ]=1
+    
+    return dict(sort_dic(frequencyDocument))
+
+def get_tf( frequency, qtTokens ):
+    if ( type(frequency) != dict ):
+        print( '\n---Tokens não é uma lista!', frequency)
+    if ( len(frequency) == 0 ):
+        print( '\n---Lista de tokens vazia!', frequency)
+    tf = {}
+    if qtTokens != 0:
+        for key in frequency:
+            tf[key] = frequency[key] / qtTokens
+    return tf
+
+def get_tfLog( frequency ):
+    if ( type(frequency) != dict ):
+        print( '\n---Tokens não é uma lista!', frequency)
+    if ( len(frequency) == 0 ):
+        print( '\n---Lista de tokens vazia!', frequency)
+    tfLog = {}
+    for key in frequency:
+        tfLog[key] = math.log(1 + frequency[key])
+    return tfLog
+
+def get_tfDouble( frequency, qtMax ):
+    if ( type(frequency) != dict ):
+        print( '\n---Tokens não é uma lista!', frequency)
+    if ( len(frequency) == 0 ):
+        print( '\n---Lista de tokens vazia!', frequency)
+    tfDouble = {}
+    if qtMax != 0:
+        for key in frequency:
+            tfDouble[key] = 0.5 + ( 0.5 * (frequency[key] / qtMax))
+    return tfDouble
+    
+if __name__ =="__main__d":
     get_text("jkak.pdf")
